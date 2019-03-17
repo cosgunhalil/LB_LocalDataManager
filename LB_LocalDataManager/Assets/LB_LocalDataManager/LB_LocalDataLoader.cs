98083be6bf5dadff8b976502fb113e54c9bc7ec6
Halil Coşgun <cosgun.halil@gmail.com>
2019-03-17 14:03:43 +0300

    loader and saver added

---
+++ LB_LocalDataManager/Assets/LB_LocalDataManager/LB_LocalDataLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LB_LocalDataLoader : MonoBehaviour
+{
+    public T LoadData<T>()
+    {
+#if UNITY_EDITOR
+        string path = Application.dataPath + "/LocalData.txt";
+#else
+        string path = Application.persistentDataPath + "/LocalData.txt";
+#endif
+        var data = ReadDataFromPath(path);
+
+        return (T)Convert.ChangeType(data, typeof(T));
+    }
+
+    public string ReadDataFromPath(string path)
+    {
+        string data = null;
+        try
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    data = reader.ReadToEnd();
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+
+        }
+
+        return data;
+    }
+
+}
---
+++ LB_LocalDataManager/Assets/LB_LocalDataManager/LB_LocalDataSaver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LB_LocalDataSaver
+{
+    public void SaveData<T>(T dataObject)
+    {
+#if UNITY_EDITOR
+        string path = Application.dataPath + "/LocalData.txt";
+#else
+        string path = Application.persistentDataPath + "/LocalData.txt";
+#endif
+
+        var data = JsonUtility.ToJson(dataObject);
+
+        try
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.Write(data);
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+            //todo send error to remote analytic tool such as Crashlytics
+        }
+    }
+
+
+}
